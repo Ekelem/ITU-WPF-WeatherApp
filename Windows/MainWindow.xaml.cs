211284c1c5039fee90a75c6e5b73e209531e27d6
Ekelem <xkelem01@stud.fit.vutbr.cz>
2019-07-29 06:46:47 +0200

    submitted version for school

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WPFWeather.Services;
+using WPFWeather.ViewModels;
+
+namespace WPFWeather.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WeatherViewModel viewModel = new WeatherViewModel(new WeatherDiskService());
+            DataContext = viewModel;
+        }
+    }
+}
